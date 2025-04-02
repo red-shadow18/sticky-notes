b67bfa7d39869b9fdd66bd3808c1e31f7d93b552
Ayush Mishra <2018056@iiitdmj.ac.in>
2025-04-03 02:09:07 +0530

    added correct backend url

--- src/apiUtils/apicalls.jsx
+++ src/apiUtils/apicalls.jsx
@@ -1,26 +1,35 @@
 import axios from "axios";
 
+const API_BASE_URL="http://localhost:8081"
+const API_PROD_BASE_URL="https://sticky-notes-am-be-production.up.railway.app"
+
+const api=axios.create({
+    baseURL:API_PROD_BASE_URL
+})
+
  const saveStickyNote=(stickyNoteData, updateStickyNotesFunction)=>{
-    axios.post("http://localhost:8081/api/saveNewStickyNote",{...stickyNoteData}).then((response)=>{
+    api.post("/api/saveNewStickyNote",{...stickyNoteData}).then((response)=>{
         updateStickyNotesFunction(response.data.updatedStickyNotes)
     })
 }
 
 const deleteStickyNote=(id,postSuccessfullDeleteFunction)=>{
-    axios.delete(`http://localhost:8081/api/delete/${id}`).then((response)=>{
+    api.delete(`/api/delete/${id}`).then((response)=>{
         postSuccessfullDeleteFunction(response.data.updatedStickyNotes)
     })
 }
 
 
  const getAllStickyNotes=(postSuccessfullfetchFunction)=>{
-    axios.get("http://localhost:8081/api/getAllStickyNotes").then((response)=>{
+    api.get("/api/getAllStickyNotes").then((response)=>{
         postSuccessfullfetchFunction(response.data.allStickyNotes)
-    })
+    }).catch(error => {
+        console.error("Error:", error.response?.status, error.response?.data);
+    });
 }
 
 const updateStickyNote=(id,updatedNote,postSuccesfullUpdateFunction)=>{
-    axios.put("http://localhost:8081/api/updateStickyNote",{...updatedNote}).then(response=>{
+    api.put("/api/updateStickyNote",{...updatedNote}).then(response=>{
         postSuccesfullUpdateFunction(response.data.updatedStickyNotes)
     })
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base:'http://localhost:8081/',
+//   plugins: [react()],
+//   server:{
+//     proxy:{
+//       "/api":{
+//         target:'http://localhost:8081',
+//         changeOrigin:true,
+//         secure:false
+//       }
+//     }
+//   }
+// })
